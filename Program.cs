be44d4db136c90a2218d0d859a9d7fc43051964b
Isaac Pugliese <isaacpugliese2010@gmail.com>
2020-09-18 23:17:55 -0300

    musica finalizada

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("precione Qualquer Tecla para iniciar");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Cabelos Arco-íris");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Kamaitachi");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Me perdi no abismo e voltei");
+            Console.WriteLine("Teu amor diminui minha vontade de querer morrer");
+            Console.WriteLine("Teu abraço é como um terapeuta que vem socorrer");
+            Console.WriteLine("Todos os medos que eu guardo mesmo sem querer");
+
+            Console.WriteLine();
+            
+            Console.WriteLine("Estou te esperando na calçada");
+            Console.WriteLine("Bem em frente a sua casa");
+            Console.WriteLine("Nessa bela madrugada");
+            Console.WriteLine("Vamos sentar nesse meio-fio");
+            Console.WriteLine("E tentar encontrar a estrela D'alva");
+            Console.WriteLine("E esquecer que segunda feira no caso amanhã já tem aula");
+            
+            Console.WriteLine();
+            
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Teus cabelos arco-íris");
+            Console.WriteLine("Me travou e me enlouqueceu");
+            Console.WriteLine("Mais perfeita que as canções de Beethoven, Mozart, Orfeu");
+            Console.WriteLine("Deusa dos meus sonhos, versão feminina de Morfeu");
+            Console.WriteLine("Labirinto de Dedálo que meu coração se perdeu");
+            Console.WriteLine("Perdeu[x5]");
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Me perdi no abismo e voltei");
+            Console.WriteLine("Teu amor diminui minha vontade de querer morrer");
+
+            // fonte: https://www.vagalume.com.br/kamaitachi/cabelos-arco-iris.html#play:all                       
+        }
+    }
+}
